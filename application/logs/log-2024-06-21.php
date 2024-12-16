1a05f056967072b7a1796746b938ccd78de7170d
Dev-ServerCake <dev.servercake@gmail.com>
2024-12-16 15:04:28 +0530

    after removed logs and media

--- application/logs/log-2024-06-21.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2024-06-21 13:38:35 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it C:\xampp\htdocs\projects\eshop-vendor\system\database\drivers\mysqli\mysqli_driver.php 211
-ERROR - 2024-06-21 13:38:35 --> Unable to connect to the database
